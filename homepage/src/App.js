03a305ab1f2f931bad70f79bd1bae4945c7581c8
ennanart <annetran1365@gmail.com>
2016-11-28 06:31:40 -0500

    removed unused code

--- homepage/src/App.js
+++ homepage/src/App.js
@@ -12,35 +12,7 @@ class App extends Component {
     super();
 
     this.state = {
-      items: {
-        1: {
-          id: 1,
-          name: 'Design Patterns: Elements of Reusable Object-Oriented Software',
-          price: 57.71,
-          quantity: 1,
-        },
-        2: {
-          id: 2,
-          name: 'React: Up & Running: Building Web Applications',
-          price: 42.81,
-          quantity: 1,
-        },
-      },
     };
-
-    this.updateQuantity = this.updateQuantity.bind(this);
-  }
-
-  updateQuantity(id, newQuantity) {
-    const newItem = Object.assign({}, this.state.items[id], {
-      quantity: newQuantity,
-    });
-
-    this.setState({
-      items: Object.assign({}, this.state.items, {
-        [id]: newItem,
-      }),
-    });
   }
 
   render() {
