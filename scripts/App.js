6baa8216473eddd8df6421dc4cb47224bae88a63
Anne <annetukhanh.tran@mail.utoronto.ca>
2016-12-02 00:28:10 -0500

    modified schema and user routes

---
+++ scripts/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'; 
+//import is ES6 equivalent to require
+var $ = require("jquery");
+
+import Navbar from './Navbar';
+import LoginSignup from './LoginSignup';
+import RecentListings from './RecentListings';
+
+require('./App.css');
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <div>
+      
+        <div>
+          <Navbar />
+          <LoginSignup />
+          <RecentListings />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
