db111cb5c9d5748c889d619b0675a5d4cfa1a160
Ke Wu <danielwuke@gmail.com>
2016-12-03 16:36:56 -0500

    ignore modules, add listings

--- public/js/index.js
+++ public/js/index.js
@@ -43,7 +43,7 @@
                             "<img src='"+data[i].picture+"' alt='listing-image' class='recent-img'>"+
                             "<div class='caption'>"+
                                 "<h3 class='pull-right'>"+data[i].price+"</h3>"+
-                                "<h3 style='text-align: left; font-size: 19px;'><a href='/listing?id="+data[i]._id+"'>"+data[i].title+"</a></h3>"+
+                                "<h3 style='text-align: left; font-size: 19px;'><a href='/listings?id="+data[i]._id+"'>"+data[i].title+"</a></h3>"+
                                 // "<p style='font-size: 12px; text-align: left'>Posted by "+data[i].user_id+" on "+data[i].date+"</p>"+
                                 "<p style='font-size: 14px; text-align: left'>"+data[i].description+"</p>"+
                             "</div>"+
