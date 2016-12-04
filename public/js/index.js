00e1cc51840baa205209e40c0001090015284e1a
Anne <annetukhanh.tran@mail.utoronto.ca>
2016-12-04 18:05:22 -0500

    finished recent listings and styling

--- public/js/index.js
+++ public/js/index.js
@@ -2,19 +2,23 @@
     "use strict"; // Start of use strict
 
     $.get('/listings', "", function(data, status) {
-        console.log(JSON.stringify(data));
-        console.log(JSON.stringify(data.sort(GetSortOrder("date"))));
+        // console.log(JSON.stringify(data));
+        // console.log(JSON.stringify(data.sort(GetSortOrder("date"))));
+
+        let sortedByDate = data.sort(GetSortOrder("date"));
         for (let i=0; i < 12; i++) {
-            if (data[i] != undefined) {
+            if (sortedByDate[i] != undefined) {
                 $("#recent").append(
                     "<div class='col-sm-4 col-lg-4 col-md-4' class='recent-row'>"+
                         "<div class='thumbnail'>"+
-                            "<img src='"+data[i].picture+"' alt='listing-image' class='recent-img'>"+
+                            "<img src='"+sortedByDate[i].picture+"' alt='listing-image' class='recent-img'>"+
                             "<div class='caption'>"+
-                                "<h3 class='pull-right'>"+data[i].price+"</h3>"+
-                                "<h3 style='text-align: left; font-size: 19px;'><a href='/listings?id="+data[i]._id+"'>"+data[i].title+"</a></h3>"+
-                                // "<p style='font-size: 12px; text-align: left'>Posted by "+data[i].user_id+" on "+data[i].date+"</p>"+
-                                "<p style='font-size: 14px; text-align: left'>"+data[i].description+"</p>"+
+                                "<h3 class='pull-right'>"+sortedByDate[i].price+"</h3>"+
+                                "<h2 style='text-align: left; font-size: 19px;'><a href='/listings?id="+
+                                sortedByDate[i]._id+"'>"+sortedByDate[i].title+"</a></h2>"+
+                                "<p style='font-size: 13px; text-align: left'>Posted on "+sortedByDate[i].date+
+                                " by <a href='/users?username="+sortedByDate[i].username+"'>"+sortedByDate[i].username+"</a> </p>"+
+                                "<p style='font-size: 15px; text-align: left' class='description'>"+sortedByDate[i].description+"</p>"+
                             "</div>"+
                         "</div>"+
                     "</div>"
@@ -25,9 +29,9 @@
 
     function GetSortOrder(prop) {  
         return function(a, b) {  
-            if (a[prop] > b[prop]) {  
+            if (a[prop] < b[prop]) {  
                 return 1;  
-            } else if (a[prop] < b[prop]) {  
+            } else if (a[prop] > b[prop]) {  
                 return -1;  
             }  
             return 0;  
