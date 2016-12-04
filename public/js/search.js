8d609e6a847c804ee8e84896641fdd346995304b
David Zhang <zhang.d@live.ca>
2016-12-04 18:08:59 -0500

    Enable sorting search results"

--- public/js/search.js
+++ public/js/search.js
@@ -6,23 +6,49 @@
         event.preventDefault();
         
         let searchText = $("#search_text_input").val().trim();
-        let searchKey = $("#search_key_input").val().toLowerCase();
+        let searchKey = $("#search_key_input").val();
+        // cache
+        localStorage['searchText'] = searchText;
+        localStorage['searchKey'] = searchKey;
 
-        $.get('/search', searchKey+'='+searchText, function(data, status){
-            window.location.replace("/search?" + searchKey+'='+searchText);
+        console.log("Search requested");
 
-            // if (data == "login success") {
-            //     window.location.replace("/");
-            // } else {
-            //     $("#error-confirm").text("Username and/or password are invalid.")
-            // }
+        let param_string = searchKey.toLowerCase()+'='+searchText.toLowerCase();
+
+        $.get('/search', param_string, function(data, status){
+            window.location.replace("/search?" + param_string);
         });
-        //TODO: validate for security? does mongo take care of this?
     });
 
     // Make results clickable
     $(".clickable-row").click(function() {
         window.document.location = $(this).data("href");
     });
+
+    // Allow sorting for results
+    $('#sort_by_input').on('change', function() {
+      console.log("Sort requested");
+        let sortBy = this.value;
+        // cache
+        localStorage['sortBy'] = sortBy;
+
+        let param_string = localStorage['searchKey'].toLowerCase()+'='+localStorage['searchText'].toLowerCase()+'&sort_by'+'='+sortBy.toLowerCase();
+        $.get('/search', param_string, function(data, status){
+          window.location.replace("/search?" + param_string);
+        });
+    })
+
+    // Set 'session' vars
+    if (localStorage['sortBy']){
+      $('#sort_by_input').val(localStorage['sortBy']);      
+    }
+
+    if (localStorage['searchKey']){
+      $('#search_key_input').val(localStorage['searchKey']);      
+    }
+
+    if (localStorage['searchText']){
+      $('#search_text_input').val(localStorage['searchText']);      
+    }
 
 })(jQuery); // End of use strict
