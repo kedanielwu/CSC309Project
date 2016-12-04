907e3b5ac01e91f1c3e65af22a869c0e684f3493
Ke Wu <danielwuke@gmail.com>
2016-12-04 16:28:14 -0500

    listing update

--- routes/listing-routes.js
+++ routes/listing-routes.js
@@ -41,20 +41,16 @@ exports.find = function(req, res) {
 
 exports.updateListing = function(req, res){
     console.log("PUT /listing received");
-    if(req.query.id){
-        if(req.query.title){
+    if(req.body._id){
+        if(req.body.title){
             Listing.update({"_id": req.query.id},
                         { $set:{"title": req.query.title}}, function(err, data){});
         }
-        if(req.query.description){
+        if(req.body.description){
                 Listing.update({"_id": req.query.id},
                         { $set:{"email": req.query.description}}, function(err, data){});          
         }
-        if(req.query.status){
-                Listing.update({"_id": req.query.id},
-                        { $set:{"email": req.query.status}}, function(err, data){});          
-        }
-        if(req.query.picture){
+        if(req.body.picture){
                 Listing.update({"_id": req.query.id},
                         { $set:{"picture": req.query.picture}}, function(err, data){});          
         }
