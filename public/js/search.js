c1a223799e23e45c0ec980c4cc95ff611adc541c
David Zhang <zhang.d@live.ca>
2016-12-04 14:10:41 -0500

    Render search results through navbar

---
+++ public/js/search.js
@@ -0,0 +1,29 @@
+(function($) {
+    "use strict"; // Start of use strict
+
+    // Attach search form(s) to backend
+    $('#search_form').submit(function(event) {
+        event.preventDefault();
+        
+        let searchText = $("#search_text_input").val().trim();
+        let searchKey = $("#search_key_input").val().toLowerCase();
+        // $(".error").text("");
+        // $("#error-confirm").text("");
+
+            console.log("Search requested");
+
+
+        $.get('/search', searchKey+'='+searchText, function(data, status){
+            console.log("Search requested");
+            window.location.replace("/search?" + searchKey+'='+searchText);
+
+            // if (data == "login success") {
+            //     window.location.replace("/");
+            // } else {
+            //     $("#error-confirm").text("Username and/or password are invalid.")
+            // }
+        });
+        //TODO: validate for security? does mongo take care of this?
+    });
+
+})(jQuery); // End of use strict
